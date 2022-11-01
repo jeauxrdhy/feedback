09811acf0eafd5548ef1b90c808ab417503d6590
jordhy <jordhy93@gmail.com>
2022-10-31 21:25:38 -0700

    created feedbackData and feedbackList

---
+++ feedback/src/Data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'it is really good'
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: 'it is really mid'
+    },
+    {
+        id: 3,
+        rating: 0,
+        text: 'it is really bad'
+    }
+]
+
+export default FeedbackData
