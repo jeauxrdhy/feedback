e5100a74e7ef870ba27d817a365e7dd31f6bc861
jordhy <jordhy93@gmail.com>
2022-11-02 17:02:49 -0700

    added basic validation to form submit

--- feedback/src/components/FeedbackForm.js
+++ feedback/src/components/FeedbackForm.js
@@ -4,8 +4,25 @@ import {useState} from 'react'
 
 function FeedbackForm() {
     const [text,setText] = useState('')
+    const [btnDisabled,setbtnDisabled] = useState(true)
+    const [message,setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === '')
+        {
+            setbtnDisabled(true)
+            setMessage(null)
+        }
+        else if(text !== '' && text.trim().length <= 10)
+        {
+            setMessage('text must be more than 10 characters')
+            setbtnDisabled(true)
+        }
+        else
+        {
+            setMessage(null)
+            setbtnDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -15,8 +32,9 @@ function FeedbackForm() {
             <h2>How would you rate your service with us?</h2>
             <div className='input-group'>
                 <input type="text" onChange={handleTextChange} placeholder='Write a review' value={text} />
-                <button type='submit'>Send</button>
+                <button type='submit' disabled={btnDisabled} >Send</button>
             </div>
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
